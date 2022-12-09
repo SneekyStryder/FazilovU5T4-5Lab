d06173974f9a96d508288e3b0bb1ec456dd2c6ef
Imran Fazilov <ifazilov3584@bths.edu>
2022-12-09 12:26:53 -0500

    period end

--- src/DogRunner.java
+++ src/DogRunner.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class DogRunner {
+public class DogRunner {
+    public static void main(String[] args) {
+
+        Dog myDog = new Dog("Toby", 10);
+        System.out.println(myDog.dogInfo());
+        System.out.println(myDog.getName());
+        System.out.println(myDog.getAge());
+        System.out.println(myDog.isSleeping());
+
+        myDog.setName("Tobias");
+        myDog.setAge(11);
+        myDog.setSleeping(true);
+        System.out.println(myDog.dogInfo());
+    }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,64 @@
-package PACKAGE_NAME;public class Student {
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int gradYear;
+    private double gpa;
+    private boolean inClub;
+
+    public Student(String first, String last, int gradYear, double gpa, boolean inClub) {
+        firstName = first;
+        lastName = last;
+        this.gradYear = gradYear;
+        this.gpa = gpa;
+        this.inClub = inClub;
+    }
+
+    // Add getter and setter methods for ALL instance variables here!
+
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public int getGradYear() {
+        return gradYear;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public boolean isInClub() {
+        return inClub;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    public void setGradYear(int gradYear) {
+        this.gradYear = gradYear;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setInClub(boolean inClub) {
+        this.inClub = inClub;
+    }
+
+    public String studentInfo() {
+        return "first name: " + firstName + ", last name: " + lastName +
+                ", grad year: " + gradYear + ", gpa: " + gpa +
+                ", is in club: " + inClub;
+    }
 }
